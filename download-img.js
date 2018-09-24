eba9a15eacbbdb5f48165bd0add6bfc942b7ee74
舟木正憲 <funakimfunaki@gmail.com>
2018-09-24 15:43:23 +0900

    ダウンロードの実装

---
+++ download-img.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const puppeteer     = require('puppeteer');
+const path          = require('path');
+const request       = require('request');
+const { promisify } = require('util');
+const fs            = require('fs');
+const delay         = require('delay');
+
+/**
+ * ファイルのダウンロードを行う
+ * @param {string} url - ダウンロードするファイルのURL
+ */
+const downloadFile = async (url) => {
+  // ダウンロードファイル名の確定.
+  const filename = url.split('/').pop();
+  // ファイルの取得.
+  const res = await promisify(request)({ method: 'GET', uri: url, encoding: null });
+  // 成功(200)したかどうか？
+  if (res.statusCode === 200) {
+    // 成功していればjsと同じフォルダーにファイル出力
+    await promisify(fs.writeFile)(path.join(__dirname, filename), res.body, 'binary');
+  } else {
+    // 失敗した場合はエラー処理.
+    throw new Error(`${res.statusCode} ダウンロードエラー`);
+  }
+};
+
+/**
+ *  メインロジック
+ */
+
+(async () => {
+
+  // Puppeteer の起動
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo  : 50,
+  });
+
+  // 新しい空のページを開く
+  const page = await browser.newPage();
+
+  // view port の設定
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+
+  // ページの遷移
+  console.log('----------------------goto');
+  await page.goto('http://ryoichi0102.hatenablog.com/');
+
+  await delay(1000); // スクレイピングする際にはアクセス間隔を1秒あける.
+
+  // 先頭の記事のurlを取得し、そのurlへ遷移
+  console.log('----------------------goto');
+  const firstPage = await page.evaluate(() => document.querySelector('#main article:nth-child(1) h1.entry-title a').href);
+  await page.goto(firstPage);
+
+  await delay(1000); // スクレイピングする際にはアクセス間隔を1秒あける.
+
+  // 各記事に対してのそれぞれの処理.
+  do {
+    console.log('----------------------do');
+    const imageUrls = await page.evaluate(() => Array.from(document.querySelectorAll('img.hatena-fotolife')).map(img => img.src));
+    for (let url of imageUrls) {
+      console.log(url);
+      console.log(`Downloading... ${url}`);
+      await downloadFile(url);
+    }
+
+    console.log('----------------------eval next');
+    // 最後の記事までたどると次へボタンは表示されないので、その場合はループを抜ける.
+    const next = await page.evaluate(() => document.querySelector('a[rel="next"]'));
+    if (next === null) {
+      break;
+    }
+
+    // 次のページを読み込む.
+    console.log('----------------------next');
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'load' }),
+      page.click('a[rel="next"]'),
+    ]);
+
+    await delay(1000); // スクレイピングする際にはアクセス間隔を1秒あける.
+  } while (true);
+
+  console.log('----------------------close');
+  await browser.close();
+})();
